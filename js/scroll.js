a2b9d5224bc762c734a57824c5b4247439801e67
gabe-brum <biel.brumluz@gmail.com>
2021-02-22 12:27:22 -0300

    arquivos-curriculo-online

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+// slider conhecimentos
+$(function () {
+    // scroll dinâmico 
+    $(function () {
+        $('nav a').click(function () {
+            var href = $(this).attr('href');
+            var offSetTop = $(href).offset().top;
+
+            $('html,body').animate({
+                'scrollTop': offSetTop
+            }, 2000);
+
+            return false;
+        })
+    });
+})
