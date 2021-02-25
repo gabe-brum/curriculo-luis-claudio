61d2690e2aeb3783ada61e5e34b04cae5139be75
Gabriel Brum da Luz <52430344+gabe-brum@users.noreply.github.com>
2021-02-24 21:32:56 -0300

    Update envio-email.php
    Correções de erros.

--- envio-email.php
+++ envio-email.php
@@ -1,41 +1,19 @@
 <?php
-// Envia um e-mail para a Ok Facilities
-/*
-$content	= "From: $nome\nTelefone: $fone\nE-mail: $email\nAssunto: $assunto\nMensagem: $mensagem";
-$recipient	= "marneicardoso.prof@gmail.com";
-$mailheader	= "From: $email\r\n";
+// Recebe os valores vindos do formulário
+$remetente	= $_POST['nome'];
+$email		= $_POST['email'];
+$fone		= $_POST['telefone'];
+$mensagem	= $_POST['mensagem'];
 
-//echo("Nome: $nome<br>E-mail: $email<br>Telefone: $fone<br>Assunto: $assunto<br>Mensagem: $mensagem");
-mail($recipient, $assunto, $content, $mailheader) or die("Êr row");
-//echo("Mensagem enviada!");
-print json_encode(array('mensagem' => 'Email successfully sent!', 'code' => 1));
-*/
-
-/*
-Este arquivo recebe os dados vindos do fornulário de Contato no index,
-e verifica se os campos obrigatórios foram preenchidos, mesmo tendo esta
-validação com jQuery, pode haver algum erro (ser desativado) e passar sem validação.
-*/
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$fone = $_POST['telefone'];
-$mensagem = $_POST['mensagem'];
 header('Content-Type: application/json');
 
-$formaDeContato = false;
-
-if (strcmp($email, "") != 0 || strcmp($telefone, "") != 0)
-{
-	$formaDeContato = true;
-}
-
-if (strcmp($nome, "") == 0 || strcmp($mensagem, "") == 0 || !$formaDeContato)
+// Verifica se os campos obrigatórios foram preenchidos
+if (strcmp($remetente, "") == 0 || strcmp($email, "") == 0 || strcmp($mensagem, "") == 0)
 {
 	print json_encode(array('message' => 'Por favor, preencha os campos', 'code' => 0));
 	exit();
 }
-
-if (strcmp($email, "") != 0)
+else
 {
 	if (!filter_var($email, FILTER_VALIDATE_EMAIL))
 	{
@@ -45,64 +23,41 @@ if (strcmp($email, "") != 0)
 }
 
 // Dados do E-mail
-$remetente = $nome;
-$destino = "luisclaudiodaluz@gmail.com";
-if (strcmp($assunto, "") == 0)
-{
-	$assunto = "Contato pelo site";
-}
-$conteudo = "<html>";
-$conteudo = "<body style='font-family: Verdana;'>";
-$conteudo .= "<h3>$assunto</h3>";
-$conteudo .= "<p style='text-decoration: none;'>";
-$conteudo .= "<b>Nome</b>: $nome<br>";
-$conteudo .= "<b>Telefone</b>: $telefone<br>";
-$conteudo .= "<b>E-mail</b>: $email<br>";
-$conteudo .= "<b>Mensagem</b>:<br>$mensagem<br><br>";
-$conteudo .= "</p>";
-$conteudo .= "</body></html>";
+$destino = "luisclaudiodaluz64@gmail.com";
 
-$headers = "From: $nome\r\n";
+// Dados do E-mail
+$conteudo = "<html>";
+$conteudo .= "<body style='font-family: Verdana;'>";
+$conteudo .= "	<h3>$assunto</h3>";
+$conteudo .= "	<p style='text-decoration: none;'>";
+$conteudo .= "		<b>Nome</b>: $remetente<br>";
+$conteudo .= "		<b>Telefone</b>: $fone<br>";
+$conteudo .= "		<b>E-mail</b>: $email<br>";
+$conteudo .= "		<b>Mensagem</b>: $mensagem<br><br>";
+$conteudo .= "	</p>";
+$conteudo .= "</body>";
+$conteudo .= "</html>";
+
+$headers = "From: $remetente<$email>\r\n";
 $headers .= "Bcc: gabrielbrumdaluz@gmail.com\r\n"; // Envia uma cópia oculta, CCO
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=utf-8\r\n";
 
-if (mail($destino, $assunto, $conteudo, $headers)) {
-	print json_encode(array('message' => 'E-mail enviado com sucesso!', 'code' => 1));
+if (mail($destino, "Contato pelo site", $conteudo, $headers)) {
+	print json_encode(
+		array('message' => 'Mensagem enviada!',
+			'code' => 1)
+	);
+	exit();
+
+} else {
+	print json_encode(
+		array('message' => 'Erro ao enviar mensagem...
+			Por favor, entre em contato pelo e-mail:
+			luisclaudiodaluz64@gmail.com',
+			'code' => 0)
+	);
 	exit();
 }
+?>
 
-/*
-$content	= "From: $nome \nTelefone: $fone\nE-mail: $email\nAssunto: $assunto\nMensagem: $mensagem";
-$recipient	= "marneicardoso.dev@gmail.com";
-//$recipient = "youremail@here.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $assunto, $content, $mailheader) or die("Error!");
-print json_encode(array('message' => 'E-mail enviado com sucesso!', 'code' => 1));
-exit();
-*/
-
-/*
-$headers = "From: $remetente <contato@dentsys.com.br>\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=utf-8\r\n";
-
-if (mail($destino, $assunto, $conteudo, $headers)) {
-	
-}
-
-// Dados do E-mail
-$remetente = "DentSYS";
-$destino = "marneicardoso.prof@gmail.com"; // Fixado para testes
-$assunto = "Cadastro de Paciente [$cadastrador]";
-$conteudo = "<html>";
-$conteudo = "<body style='font-family: Verdana; font-size: 16px;'>";
-$conteudo .= "<h4>Cadastro de Paciente</h4>";
-$conteudo .= "<p style='text-decoration: none;'>";
-$conteudo .= "Nome: $nome $sobrenome<br>";
-$conteudo .= "E-mail: $email, Telefone: $fone1<br>";
-$conteudo .= "Endereço: $endereco, $numero $complemento<br>";
-$conteudo .= "$bairro - $nomeCidade<br><br>";
-$conteudo .= "</p>";
-$conteudo .= "</body></html>";
-*/
